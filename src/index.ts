20fbd80aae4be36b1ba098b5c80bc905a932c8b4
prathmesh <prathameshk990@gmail.com>
2025-02-01 15:24:20 +0530

    Node modules

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import z from "zod";
+
+//Signup
+export const signupInput = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+  name: z.string().optional(),
+});
+
+//Signin
+export const signin = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+//create blog
+export const createBlog = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+//update Blog
+export const updateBlog = z.object({
+  title: z.string().optional(),
+  content: z.string().optional(),
+  id: z.string(),
+});
+
+export type signupInput = z.infer<typeof signupInput>;
+export type signin = z.infer<typeof signin>;
+export type createBlog = z.infer<typeof createBlog>;
+export type updateBlog = z.infer<typeof updateBlog>;
